fd002b5b5fc9228919ef070c9455124c78f9010e
tashinahmed <tahmed153109@bscse.uiu.ac.bd>
2020-02-29 09:48:55 +0600

    yo

--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
 import os
-import pandas as pd
-from sklearn import ensemble, preprocessing
+import pandas as  pd
+from sklearn import ensemble
+from sklearn import preprocessing
+from sklearn import metrics
 
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
 FOLD = int(os.environ.get("FOLD"))
-FOLD = None
 
 FOLD_MAPPING = {
     0: [1,2,3,4],
@@ -12,7 +13,7 @@ FOLD_MAPPING = {
     2: [0,1,3,4],
     3: [0,1,2,4],
     4: [0,1,2,3]
-}
+} 
 
 if __name__ == "__main__":
     df = pd.read_csv(TRAINING_DATA)
@@ -22,27 +23,23 @@ if __name__ == "__main__":
     ytrain = train_df.target.values
     yvalid = valid_df.target.values
 
-    train_df = train_df.drop(["id","target","kfold"], axis=1)
-    valid_df = valid_df.drop(["id","target","kfold"], axis=1)
+    train_df = train_df.drop(["id","target","kfold"],axis=1)
+    valid_df = valid_df.drop(["id","target","kfold"],axis=1)
 
     valid_df = valid_df[train_df.columns]
 
     label_encoders = []
     for c in train_df.columns:
-        lbl = preprocessing.labelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist() + df_test[c].values.tolist())
-        train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
-        valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
-        label_encoders[c] = lbl
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        train_df.loc[:,c] = lbl.transform(train_df[c].values.tolist())
+        valid_df.loc[:,c] = lbl.transform(valid_df[c].values.tolist())
+        label_encoders.append((c,lbl))
     
     # data is ready to train
-    clf = dispatcher.MODELS[MODEL]
-    clf.fit(train_df, ytrain)
-    preds = clf.predict_proba(valid_df)[:, 1]
-    print(metrics.roc_auc_score(yvalid, preds))
+    clf = ensemble.RandomForestClassifier(n_jobs=-1,verbose=2)
+    clf.fit(train_df,ytrain)
+    preds = clf.predict_proba(valid_df)[:,1]
+    print(metrics.roc_auc_score(yvalid,preds))
+    # print(preds)
 
-    joblib.dump(label_encoders, f"models/{MODEL}_{FOLD}_label_encoder.pkl")
-    joblib.dump(clf, f"models/{MODEL}_{FOLD}.pkl")
-    joblib.dump(train_df.columns, f"models/{MODEL}_{FOLD}_columns.pkl")
-
- 
